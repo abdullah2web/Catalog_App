eebc7951b280c2db10a26eb2710cd23653f2c42c
Abdullah ALShayie <36479470+abdullah2web@users.noreply.github.com>
2019-05-03 16:38:15 +0300

    Update db_setup.py

--- db_setup.py
+++ db_setup.py
@@ -65,7 +65,8 @@ class Item(Base):
         }
 
 
-engine = create_engine('sqlite:///catalogitem.db')
+#engine = create_engine('sqlite:///catalogitem.db')
+engine = create_engine('postgresql://catalog:password@localhost/catalog')
 
 
 Base.metadata.create_all(engine)
